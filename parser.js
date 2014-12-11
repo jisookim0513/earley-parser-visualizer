2429bc24cc3a22a261b201f0a5d15ff390aed6d1
jisookim0513 <jisookim@Jisoos-MacBook-Pro-2.local>
2014-12-11 00:46:34 -0800

    added parse function; still need to finish it; also changed index.html so it draws complete graph

--- parser.js
+++ parser.js
@@ -4,11 +4,49 @@
  * { “type”: “input”,
  *   “value”: “1 + 1”
  * }
- * grammar E -> E ; E
  * { “type”: “grammar”,
  *   “lhs” : “E”,
  *   “rhs” : “E;E”,
  *   “dprec” : null,
  *   “prec”: null,
  * }
+ * assoc +
+ * { "type": "assoc", 
+ *   "procedures": ["+"]
+ * };
  */
+
+ // handles only Grammar, Input, and Assoc for now
+ function parse(text){
+ 	var ast = {}
+ 	var splited = text.split(" ");
+ 	switch(splited[0].toLowerCase()) {
+ 		case "grammar":
+ 			ast["type"] = "grammar";
+ 			ast = handleGrammar(text.substring(7, text.length), ast);
+ 			break;
+
+ 		case "input":
+ 			ast["type"] = "input";
+ 			ast["value"] = text.substring(6, text.length);
+ 			break;
+
+ 		case "assoc":
+ 			ast["type"] = "assoc";
+ 			ast = handleAssoc(text.substring(6, text.length), ast);
+ 			break;
+
+ 		default: 
+			throw new ExecError("Unknown input type: " + splited[0]);
+			break;
+ 	}
+ 	return ast;
+ }
+
+ function handleGrammar(text, ast){
+
+ }
+
+ function handleAssoc(text, ast){
+
+ }
